f44ba29e6cc15c90bbfbad13d8e25fd24ab3ee0d
Joey Sisk <jjmsisk@gmail.com>
2020-11-08 22:00:27 -0600

    first attempt at coloring text fields

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const currentHour = moment().format("H");
+let userTextArr = ["", "", "", "", "", "", "", "", ""];
 
-$("#currentDay").text(momment().format("MMM Do, YYYY"));
 
+$("#currentDay").text(moment().format("MMM Do, YYYY"));
+
+timeColor();
+
+function timeColor() {
+  for (let i = 0; i < userTextArr.length; i++) {
+
+    let inputLocation = "$('#" + (i + 9) + "')";
+
+    console.log(inputLocation);
+
+    if ((i + 9) < parseInt(currentHour)) {
+      inputLocation.setAttribute("class", "past");
+    }
+
+    else if ((i + 9) === parseInt(currentHour)) {
+      inputLocation.setAttribute("class", "present");
+    }
+
+    else if ((i + 9) > parseInt(currentHour)) {
+      inputLocation.setAttribute("class", "future");
+    }
+
+  }
+}
