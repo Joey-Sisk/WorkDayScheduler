1c3e0cab28fa6050a77fe767d9ded116d7953353
Joey Sisk <jjmsisk@gmail.com>
2020-11-11 19:19:17 -0600

    basic local storage scipt written. needs more testing

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 // const currentHour = moment().format("H");
 const currentHour = 14;
-let userTextArr = ["", "", "", "", "", "", "", "", ""];
-
+let userTextArr;
 
 $("#currentDay").text(moment().format("MMM Do, YYYY"));
 
+checkStorage();
+
 timeColor();
+
+function checkStorage() {
+  let previously = localStorage.getItem("usersInput");
+  if (previously === null) {
+    userTextArr = ["", "", "", "", "", "", "", "", ""];
+  }
+  else {
+    userTextArr = previously;
+    saveInput();
+  }
+}
 
 function timeColor() {
   for (let i = 0; i < userTextArr.length; i++) {
@@ -15,11 +27,9 @@ function timeColor() {
     if ((i + 9) < parseInt(currentHour)) {
       inputLocation.attr("class", "past");
     }
-
     else if ((i + 9) === parseInt(currentHour)) {
       inputLocation.attr("class", "present");
     }
-
     else {
       inputLocation.attr("class", "future");
     }
@@ -27,7 +37,15 @@ function timeColor() {
 }
 
 function saveInput(event) {
-  console.log($(event.target).siblings().eq(1).val());
+  for (let i = 0; i < userTextArr.length; i++) {
+    const listPosition = userTextArr[i];
+
+    currentHour[i] = $(event.target).siblings().eq(1).val();
+
+    $(`#${i + 9}`).html(listPosition);
+
+    localStorage.setItem("usersInput", userTextArr);
+  }
 }
 
 $(".saveBtn").on("click", saveInput);
